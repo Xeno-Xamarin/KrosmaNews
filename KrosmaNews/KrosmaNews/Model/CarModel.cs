944d2e9b4af9763d0b9613e5fa63650658f3cef4
PARENT Jean-Marie <parent.jeanmarie@hotmail.com>
2018-02-01 15:20:44 +0100

    Ajout FreshEssential

---
+++ KrosmaNews/KrosmaNews/Model/CarModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KrosmaNews.Model
+{
+    public class CarModel
+    {
+	    public string ModelName { get; set; }
+	    public int SerialNumber { get; set; }
+	}
+}
--- KrosmaNews/KrosmaNews/PageModels/AboutPageModel.cs
+++ KrosmaNews/KrosmaNews/PageModels/AboutPageModel.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using FreshMvvm;
+using KrosmaNews.Model;
 using PropertyChanged;
+using Xamarin.Forms;
 
 namespace KrosmaNews.PageModels
 {
@@ -10,5 +13,34 @@ namespace KrosmaNews.PageModels
 	public class AboutPageModel : FreshBasePageModel
     {
 	    public string Bienvenue { get; set; } = " salut ! ";
-    }
+
+		public ObservableCollection<CarModel> MyCars { get; set; } = new ObservableCollection<CarModel>()
+		{
+			new CarModel()
+			{
+				ModelName = "Voiture 1",
+				SerialNumber = 164,
+			},
+			new CarModel()
+			{
+				ModelName = "Voiture 2",
+				SerialNumber = 654564,
+			},
+		};
+	    public Command ItemTapCommand
+	    {
+		    get
+		    {
+			    return new Command((o) =>
+			    {
+				    var item = o as CarModel;
+				    if (item != null)
+				    {
+					    CoreMethods.DisplayAlert("Title", $"You click the {item.ModelName} item","ok","cancel" );
+					    
+				    }
+			    });
+		    }
+	    }
+	}
 }
